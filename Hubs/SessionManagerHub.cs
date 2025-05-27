a52b67462c86309023fd43f171251a5e75fda70c
marcusbullock <marcus.bullock@bcgc.com>
2025-05-27 11:02:24 +0100

    basic signalr service implemented

---
+++ Hubs/SessionManagerHub.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
+
+namespace PlanningPokerSignalR.Hubs;
+
+public class SessionManagerHub : Hub
+{
+    public async Task JoinSession(string sessionCode)
+    {
+        Debug.WriteLine($"SignalR group created for session {sessionCode}");
+        await Groups.AddToGroupAsync(Context.ConnectionId, sessionCode);
+        Debug.WriteLine($"Client with ConnectionId {Context.ConnectionId} joined session: {sessionCode}");
+    }
+
+    public async Task PlayerJoined(string sessionCode)
+    {
+        Debug.WriteLine($"Player joined session {sessionCode}");
+        await Clients.Group(sessionCode).SendAsync("playerJoined", sessionCode);
+    }
+    public async Task SessionActive(string sessionCode)
+    {
+        await Clients.Group(sessionCode).SendAsync("sessionActive", sessionCode);
+    }
+
+    public async Task VoteCast(string sessionCode, int userId)
+    {
+        await Clients.Group(sessionCode).SendAsync("voteCast", sessionCode, userId);
+    }
+
+    public async Task ResetVotes(string sessionCode)
+    {
+        await Clients.Group(sessionCode).SendAsync("resetVotes", sessionCode);
+    }
+
+    public async Task ShowVotes(string sessionCode)
+    {
+        await Clients.Group(sessionCode).SendAsync("showVotes", sessionCode);
+    }
+    public async Task PlayerHighlight(string sessionCode, int userId)
+    {
+        await Clients.Group(sessionCode).SendAsync("playerHighlight", sessionCode, userId);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+using PlanningPokerSignalR.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register services before building the app
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Cors
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+// Enable Swagger middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseCors();
+app.MapControllers();
+app.MapHub<SessionManagerHub>("/realtime");
+
+app.Run();
